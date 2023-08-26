8b5c40db6bf643a461ecc78d6fdb439e4a4b6deb
Konrad <konrad.wojtylo.9@gmail.com>
2023-08-26 12:47:57 +0200

    Created exercise about standard tags

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as CSSCascadesBlockRouterMetaData } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as blockRouterMetaDataTextFundaments } from './TextFundaments/router-data';
+import { blockRouterMetaData as blockRouterMetaDataStandardTags } from './StandardTags/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaDataTextFundaments,
+  blockRouterMetaDataStandardTags,
   CSSCascadesBlockRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
