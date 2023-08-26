602d0ff3f1e1e25535096c567f99f1b394bfcc04
Konrad <konrad.wojtylo.9@gmail.com>
2023-08-26 15:49:39 +0200

    Created images exercise

--- src/App/Exercises/HtmlCss/ImageFiles/index.jsx
+++ src/App/Exercises/HtmlCss/ImageFiles/index.jsx
@@ -1,5 +1,13 @@
 import './styles.css';
 
 export function ImageFiles() {
-  return <p>ImageFiles</p>;
+  return (
+    <div>
+      <img
+        className="exercises-img-element"
+        src="https://premium4animals.pl/upload/premium4/blog//Kot-bengalski-brazowy.jpg"
+        alt="Idący kot"
+      />
+    </div>
+  );
 }
