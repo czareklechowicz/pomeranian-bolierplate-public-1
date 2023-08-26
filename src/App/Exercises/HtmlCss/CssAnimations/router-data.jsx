95322ab7a0db771965fcef802c8c8a819ac030e1
Konrad <konrad.wojtylo.9@gmail.com>
2023-08-26 14:09:43 +0200

    Created font and animation exercises

---
+++ src/App/Exercises/HtmlCss/CssAnimations/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { CssAnimations } from './index';
+
+export const blockRouterMetaData = {
+  path: 'css-animations',
+  date: '26-08-2023',
+  linkLabel: 'HTML & CSS - Animacje CSS',
+  blockNo: 11,
+  element: <CssAnimations />,
+  tags: ['text', 'css', 'animations', 'attribute'],
+};
---
+++ src/App/Exercises/HtmlCss/GoogleFonts/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { GoogleFonts } from './index';
+
+export const blockRouterMetaData = {
+  path: 'google-fonts',
+  date: '26-08-2023',
+  linkLabel: 'HTML & CSS - Google Fonts',
+  blockNo: 1,
+  element: <GoogleFonts />,
+  tags: ['attribute', 'text', 'css', 'fonts'],
+};
