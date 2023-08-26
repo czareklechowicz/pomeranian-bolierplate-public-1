d5983f5b0f478689fb8443a6a5d2a9e1d1035c72
Konrad <konrad.wojtylo.9@gmail.com>
2023-08-26 11:54:37 +0200

    Created  text-fundaments exercise

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as CSSCascadesBlockRouterMetaData } from './SelectorsAndCascade/router-data';
+import { blockRouterMetaData as blockRouterMetaDataTextFundaments } from './TextFundaments/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
+  blockRouterMetaDataTextFundaments,
   CSSCascadesBlockRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
