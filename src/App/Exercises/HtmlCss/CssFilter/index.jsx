21cfc28309c34eb3b44210a76d40041b66745cda
Konrad <konrad.wojtylo.9@gmail.com>
2023-08-27 10:51:47 +0200

    Added round image component to example exercise for clear the tree

--- src/App/Exercises/HtmlCss/CssFilter/index.jsx
+++ src/App/Exercises/HtmlCss/CssFilter/index.jsx
@@ -1,10 +1,8 @@
-import { RoundImage } from '../../../Components/RoundImage';
 import './styles.css';
 
 export function CssFilter() {
   return (
     <>
-      <RoundImage url="https://media.licdn.com/dms/image/D4E03AQHREH3X6V-64Q/profile-displayphoto-shrink_800_800/0/1684427938346?e=2147483647&v=beta&t=nsfR5W3Mi-du6tI1pZsn7_Ouar0KhMaDltWJu23MnyA" />
       <div>
         <div className="background-element"></div>
         <div className="background-element"></div>
--- src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/index.jsx
+++ src/App/Exercises/HtmlCss/Exercise-example-12-12-2023-1/index.jsx
@@ -1,5 +1,15 @@
+import { RoundImage } from '../../../Components/RoundImage';
+
 import './styles.css';
 
 export function Exercise() {
-  return <p>Ćwiczenia z HTML & CSS - 1</p>;
+  return (
+    <>
+      <RoundImage
+        size="100px"
+        url="https://media.licdn.com/dms/image/D4E03AQHREH3X6V-64Q/profile-displayphoto-shrink_800_800/0/1684427938346?e=2147483647&v=beta&t=nsfR5W3Mi-du6tI1pZsn7_Ouar0KhMaDltWJu23MnyA"
+      />
+      <RoundImage url="https://media.licdn.com/dms/image/D4E03AQHREH3X6V-64Q/profile-displayphoto-shrink_800_800/0/1684427938346?e=2147483647&v=beta&t=nsfR5W3Mi-du6tI1pZsn7_Ouar0KhMaDltWJu23MnyA" />
+    </>
+  );
 }
