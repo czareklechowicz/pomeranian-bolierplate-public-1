8c774994e2758aeed5742efef2820e129ccbab26
Konrad <konrad.wojtylo.9@gmail.com>
2023-08-27 10:17:23 +0200

    Created round image component and added it to css filter for debugging

---
+++ src/App/Components/RoundImage/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export const RoundImage = ({ url }) => {
+  return (
+    <div
+      className="round-image"
+      style={{ backgroundImage: `url(${url})` }}
+    ></div>
+  );
+};
--- src/App/Exercises/HtmlCss/CssFilter/index.jsx
+++ src/App/Exercises/HtmlCss/CssFilter/index.jsx
@@ -1,12 +1,16 @@
+import { RoundImage } from '../../../Components/RoundImage';
 import './styles.css';
 
 export function CssFilter() {
   return (
     <>
-      <div className="background-element"></div>
-      <div className="background-element"></div>
-      <div className="background-element"></div>
-      <div className="background-element"></div>
+      <RoundImage url="https://media.licdn.com/dms/image/D4E03AQHREH3X6V-64Q/profile-displayphoto-shrink_800_800/0/1684427938346?e=2147483647&v=beta&t=nsfR5W3Mi-du6tI1pZsn7_Ouar0KhMaDltWJu23MnyA" />
+      <div>
+        <div className="background-element"></div>
+        <div className="background-element"></div>
+        <div className="background-element"></div>
+        <div className="background-element"></div>
+      </div>
     </>
   );
 }
